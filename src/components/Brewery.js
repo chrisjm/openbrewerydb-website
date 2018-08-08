a87892c92ed62738d4fe94f2b2d3b680ade3887a
Chris Mears <chris.mears@gmail.com>
2018-08-08 10:12:13 -0700

    Add Autosuggest component

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -4,13 +4,37 @@ import PropTypes from 'prop-types'
 class Brewery extends PureComponent {
   render() {
     const { brewery } = this.props
+    let bgColor
+
+    switch(brewery.brewery_type) {
+      case 'micro':
+        bgColor = 'bg-green text-white'
+        break
+      case 'planning':
+        bgColor = 'bg-grey-lightest text-grey-light'
+        break
+      case 'brewpub':
+        bgColor = 'bg-orange text-white'
+        break
+      default:
+        bgColor = 'bg-grey text-grey-dark'
+    }
+
     return (
-      <li className="p-4 mb-2 rounded bg-grey-light">
-        <span>{brewery.name}, </span>
-        <span>{brewery.address}, </span>
-        <span>{brewery.city}, </span>
-        <span>{brewery.state}</span>
-      </li>
+      <div className={`p-4 mb-2 rounded ${bgColor}`}>
+        { (Object.keys(brewery).length !== 0) ?
+          <div>
+            <span>{brewery.name}, </span>
+            { brewery.address !== '' ? <span>{brewery.address}, </span> : '' }
+            { brewery.city !== '' ? <span>{brewery.city}, </span> : '' }
+            { brewery.state !== '' ? <span>{brewery.state}, </span> : '' }
+            { brewery.postal_code !== '' ? <span>{brewery.postal_code}, </span> : '' }
+            <span>({brewery.brewery_type})</span>
+          </div>
+          :
+          <span>No breweries found.</span>
+        }
+      </div>
     )
   }
 }
--- src/containers/BrewerySearch.js
+++ src/containers/BrewerySearch.js
@@ -1,58 +1,99 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import BreweryList from '../components/BreweryList'
-import SearchForm from './SearchForm'
+import { debounce } from 'throttle-debounce'
+import Autosuggest from 'react-autosuggest'
+import Brewery from '../components/Brewery';
+
+const API_SERVER_HOST = process.env.REACT_APP_API_SERVER_HOST
+
+const getSuggestionValue = suggestion => suggestion.name;
+
+const renderSuggestion = suggestion => (
+  <div className="text-lg p-4">
+    {suggestion.name}
+  </div>
+);
 
 class BrewerySearch extends Component {
-  constructor(props) {
-    super(props)
-    this.handleSearch = this.handleSearch.bind(this)
+  constructor() {
+    super();
+
+    this.debouncedGetSuggestions = debounce(500, this.getSuggestions)
+
+    this.state = {
+      value: '',
+      brewery: {},
+      suggestions: []
+    }
   }
-  
-  state = {
-    breweries: []
-  }
 
-  handleSearch(query) {
-    const params = { q: query }
+  getSuggestions = value => {
+    const params = { query: value }
 
-    console.log(params)
+    console.log(`GET breweries/autocomplete?query=${params.query}`)
 
-    axios.get(`https://api.openbrewerydb.org/breweries`, { params: params })
+    axios.get(`${API_SERVER_HOST}/breweries/autocomplete`, { params: params })
       .then(res => {
-        const breweries = res.data
-        this.setState({ breweries })
+        this.setState({ suggestions: res.data })
       })
       .catch(error => {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log("Request made")
-          console.log(error.response.data)
-          console.log(error.response.status)
-          console.log(error.response.headers)
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log("Request made no response")
-          console.log(error.request)
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Request error")
-          console.log('Error', error.message)
-        }
-        console.log(error.config)
+        this.setState({ suggestions: [] })
+        console.log(error)
+      })
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.debouncedGetSuggestions(value)
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  onSuggestionSelected = (_event, { suggestion }) => {
+    const brewery = suggestion
+
+    console.log(`GET breweries/${brewery.id}`)
+
+    axios.get(`${API_SERVER_HOST}/breweries/${brewery.id}`)
+      .then(res => {
+        this.setState({ brewery: res.data })
+      })
+      .catch(error => {
+        console.log(error)
       })
   }
 
   render() {
-    const { breweries } = this.state
+    const { brewery, suggestions, value } = this.state
+
+    const inputProps = {
+      placeholder: 'Type a brewery name',
+      value,
+      onChange: this.onChange,
+      className: 'shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline mb-4'
+    };
 
     return (
-      <div>
-        <SearchForm handleSubmit={this.handleSearch} />
-        <BreweryList breweries={breweries} />
+      <div className="mb-4">
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+          onSuggestionSelected={this.onSuggestionSelected}
+        />
+        <Brewery brewery={brewery} />
       </div>
     )
   }
