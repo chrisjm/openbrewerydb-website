30c6f8bc524f0fafa8f45137b86521d4bf95ea73
Chris Mears <chris.mears@gmail.com>
2018-08-24 10:04:00 -0700

    Update search with geolocation data and map link 🗺

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -4,37 +4,77 @@ import PropTypes from 'prop-types'
 class Brewery extends PureComponent {
   render() {
     const { brewery } = this.props
+    const {
+      name,
+      street,
+      city,
+      state,
+      postal_code,
+      country,
+      longitude,
+      latitude,
+      brewery_type
+    } = brewery
     let bgColor
+    let address
+
+    if (city !== '' && state !== '') {
+      if (street === '')
+        address = encodeURIComponent(`${name}, ${city}, ${state}`)
+      else {
+        address = encodeURIComponent(`${name}, ${street}, ${city}, ${state}`)
+      }
+    } else {
+      address = null;
+    }
 
     switch(brewery.brewery_type) {
       case 'micro':
+      case 'regional':
+      case 'large':
         bgColor = 'bg-green text-white'
-        break
-      case 'planning':
-        bgColor = 'bg-grey-lightest text-grey-light'
         break
       case 'brewpub':
         bgColor = 'bg-orange text-white'
         break
       default:
-        bgColor = 'bg-grey text-grey-dark'
+        bgColor = 'bg-grey-light text-grey'
     }
 
     return (
       <div className={`p-4 mb-2 rounded ${bgColor}`}>
         { (Object.keys(brewery).length !== 0) ?
-          <address className="roman">
-            <div className="text-lg mb-2">
-              <span className="font-bold">{brewery.name} </span>
-              <span>({brewery.brewery_type})</span>
-            </div>
-            { brewery.address !== '' ? <div>{brewery.address}</div> : '' }
-            <div>
-              { brewery.city !== '' ? <span>{brewery.city}, </span> : '' }
-              { brewery.state !== '' ? <span>{brewery.state}, </span> : '' }
-              { brewery.postal_code !== '' ? <span>{brewery.postal_code} </span> : '' }
-            </div>
-          </address>
+          <div>
+            <address className="roman">
+              <div className="text-lg mb-2">
+                <span className="font-bold">{name}</span>
+                <span> ({brewery_type})</span>
+              </div>
+              { street !== '' ? <div>{street}</div> : '' }
+              <div>
+                { city !== '' ? <span>{city}, </span> : '' }
+                { state !== '' ? <span>{state}, </span> : '' }
+                { postal_code !== '' ? <span>{postal_code}, </span> : '' }
+                { country !== '' ? <span>{country} </span> : '' }
+              </div>
+              <div>
+                Geo Coordinates: {latitude}/{longitude}
+              </div>
+            </address>
+            { address ?
+              <div>
+                <a
+                  class="text-white"
+                  href={`https://www.google.com/maps/search/?api=1&query=${address}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Google Map
+                </a>
+              </div>
+              : ''
+            }
+          </div>
           :
           <span>No brewery selected.</span>
         }
